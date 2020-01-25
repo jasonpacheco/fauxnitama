9e16e090215a4db5028b711a5a63d36d47b3f714
Jason Pacheco <pacheco.jason93@gmail.com>
2020-01-25 13:53:29 -0800

    Grid produced

--- src/components/Board/Board.tsx
+++ src/components/Board/Board.tsx
@@ -1,19 +1,41 @@
 import React from 'react';
 import styled from 'styled-components';
-import { getIds } from '../../utils';
+import { getIds, BOARD_GAME } from '../../utils';
+const { COLS } = BOARD_GAME;
+
+const BoardWrapper = styled.div`
+  margin: 0 auto;
+  width: 30rem;
+`;
 
 const Grid = styled.div`
+  border: 1px solid #000;
   display: grid;
-  grid-template-columns: repeat(5, 1fr);
+  height: 30rem;
+  grid-template-columns: repeat(${COLS}, 1fr);
+`;
+
+const Box = styled.div`
+  border: 1px solid #000;
+  &:hover {
+    cursor: pointer;
+  }
 `;
 
 const Board: React.FC = () => {
   return (
-    <Grid>
-      {getIds().map(({ id, x, y }) => (
-        <div key={id}>{`{x:${x},y:${y},id:${id}}`}</div>
-      ))}
-    </Grid>
+    <BoardWrapper>
+      <Grid>
+        {getIds().map(({ id, x, y }) => (
+          <Box
+            key={id}
+            onClick={(): void => console.log(`{x:${x},y:${y},id:${id}}`)}
+          >
+            Box
+          </Box>
+        ))}
+      </Grid>
+    </BoardWrapper>
   );
 };
 
